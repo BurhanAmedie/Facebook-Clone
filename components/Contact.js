68f1bdbe62e92fa535b7b517db03d521413af69c
Deanbarber <dib47620@uga.edu>
2021-05-23 15:36:54 -0400

    auth correction

--- components/Contact.js
+++ components/Contact.js
@@ -3,9 +3,9 @@ import Image from "next/image"
 
 function Contact({src, name}) {
     return (
-        <div className="flex items-center space-x-3 mb-2 relative hover:bg-gray-200 cursor-ponter p-2 rounded-xl">
+        <div className="flex items-center space-x-3 mb-2 relative hover:bg-white cursor-ponter p-2 rounded-xl cursor-pointer border-l-2 border-gray-400">
             <Image
-            className="rounded-full"
+            className="rounded-full "
             src ={src} 
             width={50}
             height={50}
